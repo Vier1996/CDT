1a7e397609069522158d05f91ed141dc873fe0c7
mrVierIos <m.prilipko@ciprosoft.com>
2024-07-18 02:19:56 +0300

    feat: add workplace variations

---
+++ Assets/InternalAssets/Codebase/Gameplay/Workplaces/WorkplaceVariations/ChillingWorkplace.cs
@@ -0,0 +1,8 @@
+using InternalAssets.Codebase.Gameplay.Workplaces.Base;
+
+namespace InternalAssets.Codebase.Gameplay.Workplaces.WorkplaceVariations
+{
+    public class ChillingWorkplace : Workplace
+    {
+    }
+}
